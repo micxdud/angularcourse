ebea65a1152186b80e80986fccdc6905b447e7a5
micxdud <dudek_michal@hotmail.com>
2016-03-22 17:21:02 +0000

    ui-router with basic conf

--- app/index.js
+++ app/index.js
@@ -1,6 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.css';
 
 import angular from 'angular';
+import uirouter from 'angular-ui-router';
+import routing from './config';
 
+const ngModule = 
+                angular
+                    .module('app', [uirouter]);
+                    .config(routing)
 
-const ngModule = angular.module('app', []);
