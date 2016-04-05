f1192a3879fcce2ae4c4c3ac94497bedeaad73e5
micxdud <dudek_michal@hotmail.com>
2016-04-05 16:18:19 +0000

    GitHub Api first draft

---
+++ app/features/github/github.controller.js
@@ -0,0 +1,5 @@
+export default class GitHubController {
+  constructor(){
+    console.log();
+  }
+  }
--- app/index.js
+++ app/index.js
@@ -1,12 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.css';
 
 import angular from 'angular';
-
 import uirouter from 'angular-ui-router';
-import routing from './config';
-import home from './features/home'
 
-const ngModule = angular.module('app', [uirouter, home])
+import routing from './config';
+
+import home from './features/home';
+import github from './features/github';
+
+const ngModule = angular.module('app', [uirouter, home, github])
                         .config(routing);
    
 
