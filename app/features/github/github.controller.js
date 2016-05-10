36f7ece092fe9bb2512029ea9ab56dfdc329ece3
micxdud <dudek_michal@hotmail.com>
2016-05-10 16:21:58 +0000

    Adds GitHubService

--- app/features/github/github.controller.js
+++ app/features/github/github.controller.js
@@ -1,21 +1,13 @@
 export default class GitHubController 
 {
-  constructor($http, $scope) {
-      this.$http = $http;
-      this.getUser();
-      this.getRepos();
+  constructor(GitHubService) {
+  GitHubService.getUser('Microsoft') 
+      .then(this.handleGetUserSuccess.bind(this));
+  
+  GitHubService.getRepos('Microsoft')
+      .then(this.handleGetReposSuccess.bind(this));
   }
 
-  getUser() {
-      this.$http.get('https://api.github.com/users/Microsoft')
-        .then(this.handleGetUserSuccess.bind(this));
-  }
-  
-  getRepos() {
-      this.$http.get('https://api.github.com/users/Microsoft/repos')
-        .then(this.handleGetReposSuccess.bind(this));
-  }
-  
   handleGetUserSuccess(response) {
       this.User = response.data;
       console.log(this.User);
--- app/features/github/index.js
+++ app/features/github/index.js
@@ -3,12 +3,14 @@ import uirouter from 'angular-ui-router';
 
 import routing from './github.routes';
 import GitHubController from './github.controller';
+import GitHubService from './github.service';
 import githubUser from './directives/github_user';
 import githubRepos from './directives/github_repos';
 
 export default angular.module('app.github', [uirouter])
                       .config(routing)
                       .controller('GitHubController', GitHubController)
+                      .service('GitHubService', GitHubService)
                       .directive('githubUser', githubUser)
                       .directive('githubRepos', githubRepos)
                       .name;
